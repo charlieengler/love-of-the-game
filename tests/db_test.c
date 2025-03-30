b265ae0d83d4355f138e33120331efe91707d8d1
Charlie Engler <charlieengler3@gmail.com>
2025-03-30 15:33:56 -0400

    Basic testing framework established

---
+++ tests/db_test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "test_tools.h"
+
+int test_database() {
+    // TODO: Set up database and run tests on it
+    printf("Testing database\n");
+
+    // TODO: Return 0 on success and something else on failure
+    return 0;
+}
