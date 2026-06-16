e3e20ad758870fc78ff9bcc2f756525b3b9b7d08
Charlie Engler <charlieengler3@gmail.com>
2026-06-16 14:19:43 -0400

    Updated main with better argument checking

--- src/main.c
+++ src/main.c
@@ -6,16 +6,9 @@
 #include "./tests/tests.h"
 
 int main(int argc, char **argv) {
-    if (argc > 2) {
+    if (argc > 1) {
         if (argc < 3 || argc > 4) {
-            // TODO: Update me as I add more tests
-            printf("Invalid number of arguments!\n");
-            printf("Program format is:\n");
-            printf("./main test <module>\n");
-            printf("    module:\n");
-            printf("        all:    run all tests\n");
-            printf("        db:     run database tests\n");
-            printf("        json:   run JSON parser tests\n");
+            goto bad_args;
         }
 
         if (strcmp(argv[2], "db") == 0) {
@@ -30,14 +23,26 @@ int main(int argc, char **argv) {
         }
 
         if (strcmp(argv[2], "all")) {
-            printf("testing error: invalid argument\n");
-            return -1;
+            goto bad_args;
         }
 
         test_db();
+        test_json();
     } else {
         initialize_server();
     }
 
     return 0;
+
+bad_args:
+    // TODO: Update me as I add more tests
+    printf("Invalid argument(s)!\n");
+    printf("Program format is:\n");
+    printf("./main test <module>\n");
+    printf("    module:\n");
+    printf("        all:    run all tests\n");
+    printf("        db:     run database tests\n");
+    printf("        json:   run JSON parser tests\n");
+
+    return -1;
 }
