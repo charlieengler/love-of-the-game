0b7610ec4b3c592942ce330d51557daea122c668
Charlie Engler <charlieengler3@gmail.com>
2026-08-21 11:25:04 -0400

    Added bet placing logic to new table framework

--- src/server/games/blackjack/blackjack.c
+++ src/server/games/blackjack/blackjack.c
@@ -7,7 +7,7 @@
 #include "../../../include/server/database/database.h"
 #include "../../../include/server/utils/json_handler.h"
 
-char *blackjack_join_table(struct database_mappings *tables_db, char *data) {
+char *blackjack_join_table(struct database_mappings *db, char *data) {
     struct json_object *parsed_data = json_parse_string(data);
 
     struct json_value *user_id_json = json_find_entry(parsed_data, "userID");
@@ -17,7 +17,7 @@ char *blackjack_join_table(struct database_mappings *tables_db, char *data) {
     char *table_id = table_id_json->str_val;
 
     // TODO: Error checking
-    struct database_entry *found_table = db_find(tables_db, table_id);
+    struct database_entry *found_table = db_find(db, table_id);
 
     if (found_table == NULL) {
         printf("blackjack error: could not find entry %s in database\n", table_id);
@@ -38,9 +38,9 @@ char *blackjack_join_table(struct database_mappings *tables_db, char *data) {
         new_table->data_ptr = (void *)json_to_string(new_table_json);
 
         // TODO: Error checking
-        db_insert(tables_db, new_table);
+        db_insert(db, new_table);
 
-        found_table = db_find(tables_db, table_id);
+        found_table = db_find(db, table_id);
     }
 
     if (found_table == NULL) {
@@ -49,62 +49,49 @@ char *blackjack_join_table(struct database_mappings *tables_db, char *data) {
 
     printf("Joined table %s as %s - %s\n", table_id, user_id, (char *)found_table->data_ptr);
 
-    json_destroy_object(parsed_data);
-
-    return (char *)found_table->data_ptr;
+    // TODO: Actual return header
+    return NULL;
 }
 
 char *blackjack_place_bet(struct database_mappings *db, char *data) {
+    struct json_object *data_json = json_parse_string(data);
+
+    struct json_value *user_id_json = json_find_entry(data_json, "userID");
+    char *user_id = user_id_json->str_val;
+
+    struct json_value *table_id_json = json_find_entry(data_json, "tableID");
+    char *table_id = table_id_json->str_val;
+
+    // TODO: Should be a number value once that is implemented
+    struct json_value *bet_json = json_find_entry(data_json, "bet");
+    char *bet = bet_json->str_val;
+
     // TODO: Error checking
-    struct database_entry *found_entry = db_find(db, "charlie");
+    blackjack_join_table(db, data);
 
-    if (found_entry == NULL) {
-        printf("blackjack error: could not find entry %s in database\n", "charlie");
-
-        struct database_entry *new_entry = (struct database_entry *)malloc(sizeof(struct database_entry));
-        struct json_object *entry_json = json_initialize_object();
-
-        struct json_value *entry_val = (struct json_value *)malloc(sizeof(struct json_value));
-        entry_val->str_val = data;
-        entry_val->type = JSON_FORMATTED_STRING;
-
-        // TODO: Unique user ID for each user to store in the database
-        json_add_entry(entry_json, "blackjack-bet", entry_val);
-
-        // TODO: This should not be hard coded either
-        new_entry->key = (char *)calloc(strlen("charlie") + 1, sizeof(char));
-        strcpy(new_entry->key, "charlie");
-        new_entry->type = DB_STRING;
-        new_entry->data_ptr = (void *)json_to_string(entry_json);
-
-        // TODO: Error checking
-        db_insert(db, new_entry);
-
-        found_entry = db_find(db, "charlie");
-    }
+    // TODO: Error checking
+    struct database_entry *found_entry = db_find(db, table_id);
 
     if (found_entry == NULL) {
         printf("blackjack error: db entry is still null\n");
     }
 
-    struct json_object *entry_json = json_initialize_object();
+    struct json_object *entry_json = json_parse_string((char *)found_entry->data_ptr);
 
-    struct json_value *entry_val = (struct json_value *)malloc(sizeof(struct json_value));
-    entry_val->str_val = data;
-    entry_val->type = JSON_FORMATTED_STRING;
+    struct json_value *entry_bets_json = json_find_entry(entry_json, "bets");
+    if (!entry_bets_json) {
+        struct json_value *bets_val = (struct json_value *)malloc(sizeof(struct json_value));
+        bets_val->str_val = bet;
+        bets_val->type = JSON_STRING;
 
-    // TODO: Unique user ID for each user to store in the database
-    json_add_entry(entry_json, "charlie-blackjack-bet", entry_val);
+        json_add_entry(entry_json, "bets", bets_val);
+    } else {
+        entry_bets_json->str_val = bet;
+    }
 
-    found_entry->type = DB_STRING;
     free(found_entry->data_ptr);
-    found_entry->data_ptr = (void *)json_to_string(entry_json);
+    found_entry->data_ptr = json_to_string(entry_json);
 
-    printf("Placed Bet: %s - %s\n", (char *)found_entry->key, (char *)found_entry->data_ptr);
-
-    // TODO: This does not account for different data types and assumes it's a string underneath
-    char *return_header = (char *)calloc(strlen(found_entry->data_ptr), sizeof(char));
-    strcpy(return_header, found_entry->data_ptr);
-
-    return return_header;
+    // TODO: Actual return header
+    return NULL;
 }
--- src/server/games/game_router.c
+++ src/server/games/game_router.c
@@ -5,32 +5,24 @@
 #include "../../include/server/games/games.h"
 
 char *route_blackjack(char *function, char *data) {
-    struct database_mappings *blackjack_tables_db = NULL;
+    struct database_mappings *blackjack_db = NULL;
 
-    if (blackjack_tables_db == NULL) {
+    if (blackjack_db == NULL) {
         // TODO: Error checking
-        db_initialize(&blackjack_tables_db, "blackjack_tables");
-    }
-
-    struct database_mappings *blackjack_bets_db = NULL;
-
-    if (blackjack_bets_db == NULL) {
-        // TODO: Error checking
-        db_initialize(&blackjack_bets_db, "blackjack_bets");
+        db_initialize(&blackjack_db, "blackjack");
     }
 
     char *return_header = NULL;
 
     if (strstr(function, "join-table") != NULL) {
         // TODO: Check for success/failure on this function (returns void right now)
-        return_header = blackjack_join_table(blackjack_tables_db, data);
+        return_header = blackjack_join_table(blackjack_db, data);
     } else if (strstr(function, "bet") != NULL) {
         // TODO: Check for success/failure on this function (returns void right now)
-        return_header = blackjack_place_bet(blackjack_bets_db, data);
+        return_header = blackjack_place_bet(blackjack_db, data);
     }
 
-    db_save(blackjack_tables_db);
-    db_save(blackjack_bets_db);
+    db_save(blackjack_db);
 
     // TODO: Return an error that the function couldn't be found if the header is still NULL at this point
     if (return_header == NULL) {
