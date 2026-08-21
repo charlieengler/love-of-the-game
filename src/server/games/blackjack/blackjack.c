b5ffbe285f662902d71b8f133f285561fa458917
Charlie Engler <charlieengler3@gmail.com>
2026-08-21 10:15:27 -0400

    Updated blackjack database functionality

--- src/server/games/blackjack/blackjack.c
+++ src/server/games/blackjack/blackjack.c
@@ -4,30 +4,30 @@
 
 #include "../../../include/server/games/blackjack/blackjack.h"
 
-#include "../../../include/server/utils/json_handler.h"
 #include "../../../include/server/database/database.h"
+#include "../../../include/server/utils/json_handler.h"
 
 char *blackjack_join_table(struct database_mappings *db, char *data) {
     // TODO: Error checking
     struct database_entry *found_table = db_find(db, "active-table");
 
-    if(found_table == NULL) {
+    if (found_table == NULL) {
         printf("blackjack error: could not find entry %s in database\n", "active-table");
 
-        struct database_entry *new_table = (struct database_entry*)malloc(sizeof(struct database_entry));
+        struct database_entry *new_table = (struct database_entry *)malloc(sizeof(struct database_entry));
         struct json_object *new_table_json = json_initialize_object();
 
-        struct json_value *new_table_val = (struct json_value*)malloc(sizeof(struct json_value));
+        struct json_value *new_table_val = (struct json_value *)malloc(sizeof(struct json_value));
         new_table_val->str_val = data;
         new_table_val->type = JSON_FORMATTED_STRING;
 
         // TODO: Unique user ID for each user to store in the database
-        json_add_entry(new_table_json, "charlie-blackjack-bet", new_table_val);
+        json_add_entry(new_table_json, "active-table", new_table_val);
 
-        new_table->key = (char*)calloc(strlen("active-table") + 1, sizeof(char));
+        new_table->key = (char *)calloc(strlen("active-table") + 1, sizeof(char));
         strcpy(new_table->key, "active-table");
-        new_table->type = DB_JSON;
-        new_table->data_ptr = (void*)json_to_string(new_table_json);
+        new_table->type = DB_STRING;
+        new_table->data_ptr = (void *)json_to_string(new_table_json);
 
         // TODO: Error checking
         db_insert(db, new_table);
@@ -35,37 +35,37 @@ char *blackjack_join_table(struct database_mappings *db, char *data) {
         found_table = db_find(db, "active-table");
     }
 
-    if(found_table == NULL) {
+    if (found_table == NULL) {
         printf("blackjack error: db entry is still null\n");
     }
 
-    printf("Joined table as charlie - %s\n", (char*)found_table->data_ptr);
+    printf("Joined table as charlie - %s\n", (char *)found_table->data_ptr);
 
-    return (char*)found_table->data_ptr;
+    return (char *)found_table->data_ptr;
 }
 
 char *blackjack_place_bet(struct database_mappings *db, char *data) {
     // TODO: Error checking
     struct database_entry *found_entry = db_find(db, "charlie");
 
-    if(found_entry == NULL) {
+    if (found_entry == NULL) {
         printf("blackjack error: could not find entry %s in database\n", "charlie");
 
-        struct database_entry *new_entry = (struct database_entry*)malloc(sizeof(struct database_entry));
+        struct database_entry *new_entry = (struct database_entry *)malloc(sizeof(struct database_entry));
         struct json_object *entry_json = json_initialize_object();
 
-        struct json_value *entry_val = (struct json_value*)malloc(sizeof(struct json_value));
+        struct json_value *entry_val = (struct json_value *)malloc(sizeof(struct json_value));
         entry_val->str_val = data;
         entry_val->type = JSON_FORMATTED_STRING;
 
         // TODO: Unique user ID for each user to store in the database
-        json_add_entry(entry_json, "charlie-blackjack-bet", entry_val);
+        json_add_entry(entry_json, "blackjack-bet", entry_val);
 
         // TODO: This should not be hard coded either
-        new_entry->key = (char*)calloc(strlen("charlie") + 1, sizeof(char));
+        new_entry->key = (char *)calloc(strlen("charlie") + 1, sizeof(char));
         strcpy(new_entry->key, "charlie");
         new_entry->type = DB_STRING;
-        new_entry->data_ptr = (void*)json_to_string(entry_json);
+        new_entry->data_ptr = (void *)json_to_string(entry_json);
 
         // TODO: Error checking
         db_insert(db, new_entry);
@@ -73,13 +73,13 @@ char *blackjack_place_bet(struct database_mappings *db, char *data) {
         found_entry = db_find(db, "charlie");
     }
 
-    if(found_entry == NULL) {
+    if (found_entry == NULL) {
         printf("blackjack error: db entry is still null\n");
     }
 
     struct json_object *entry_json = json_initialize_object();
 
-    struct json_value *entry_val = (struct json_value*)malloc(sizeof(struct json_value));
+    struct json_value *entry_val = (struct json_value *)malloc(sizeof(struct json_value));
     entry_val->str_val = data;
     entry_val->type = JSON_FORMATTED_STRING;
 
@@ -88,12 +88,12 @@ char *blackjack_place_bet(struct database_mappings *db, char *data) {
 
     found_entry->type = DB_STRING;
     free(found_entry->data_ptr);
-    found_entry->data_ptr = (void*)json_to_string(entry_json);
+    found_entry->data_ptr = (void *)json_to_string(entry_json);
 
-    printf("Placed Bet: %s - %s\n", (char*)found_entry->key, (char*)found_entry->data_ptr);
+    printf("Placed Bet: %s - %s\n", (char *)found_entry->key, (char *)found_entry->data_ptr);
 
     // TODO: This does not account for different data types and assumes it's a string underneath
-    char *return_header = (char*)calloc(strlen(found_entry->data_ptr), sizeof(char));
+    char *return_header = (char *)calloc(strlen(found_entry->data_ptr), sizeof(char));
     strcpy(return_header, found_entry->data_ptr);
 
     return return_header;
--- src/server/games/game_router.c
+++ src/server/games/game_router.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include "../../include/server/games/games.h"
 #include "../../include/server/database/database.h"
-
-// TODO: Initilialize database for each service that requires it and search disk for previously stored instance of said databases
-struct database_mappings *blackjack_db = NULL;
+#include "../../include/server/games/games.h"
 
 char *route_blackjack(char *function, char *data) {
-    if(blackjack_db == NULL) {
+    struct database_mappings *blackjack_db = NULL;
+
+    if (blackjack_db == NULL) {
         // TODO: Error checking
         db_initialize(&blackjack_db, "blackjack");
     }
 
     char *return_header = NULL;
 
-    if(strstr(function, "join-table") != NULL) {
+    if (strstr(function, "join-table") != NULL) {
         // TODO: Check for success/failure on this function (returns void right now)
         return_header = blackjack_join_table(blackjack_db, data);
-    } else if(strstr(function, "bet") != NULL) {
+    } else if (strstr(function, "bet") != NULL) {
         // TODO: Check for success/failure on this function (returns void right now)
         return_header = blackjack_place_bet(blackjack_db, data);
     }
@@ -27,10 +25,10 @@ char *route_blackjack(char *function, char *data) {
     db_save(blackjack_db);
 
     // TODO: Return an error that the function couldn't be found if the header is still NULL at this point
-    if(return_header == NULL) {
+    if (return_header == NULL) {
         // TODO: Better return header error
         const char *header_const = "{\"error\":\"Make me more descriptive\"}";
-        return_header = (char*)calloc(strlen(header_const) + 1, sizeof(char));
+        return_header = (char *)calloc(strlen(header_const) + 1, sizeof(char));
         strcpy(return_header, header_const);
     }
 
@@ -40,14 +38,14 @@ char *route_blackjack(char *function, char *data) {
 // TODO: Return the response to the route request
 char *route_game(char *path, char *data) {
     char *route = NULL;
-    if((route = strstr(path, "blackjack/")) != NULL) {
+    if ((route = strstr(path, "blackjack/")) != NULL) {
         char *res = route_blackjack(route + strlen("blackjack/"), data);
 
         return res;
     }
 
     const char *header_const = "{\"error\":\"Unknown game\"}";
-    char *return_header = (char*)calloc(strlen(header_const) + 1, sizeof(char));
+    char *return_header = (char *)calloc(strlen(header_const) + 1, sizeof(char));
     strcpy(return_header, header_const);
 
     return return_header;
